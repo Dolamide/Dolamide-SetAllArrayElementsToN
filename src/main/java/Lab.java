5fa7f9fc3344887da8e6774e11ae773016fa271a
Deborah Campbell <deborahcampbell@yahoo.com>
2023-03-04 10:32:34 +0000

    Coding Challenge: Set All Array Elements To N

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -23,6 +23,14 @@ public class Lab {
      * @return nothing, because of pass-by-reference, all changes will be reflected in the original array.
      */
     public void set(int[] arr, int n){
+        for(int i = 0; i < arr.length; i++){
+            arr [i] = n;
+            System.out.println(arr[i]);
+        }
+        // for(n = 0; n < arr.length; n++){
+        //     arr = new int[n];
+        //     System.out.println(arr[n]);
+        // }
 
     }
 }
